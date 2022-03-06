897ac4bd86d7c7c0740e1aa929b46a89d9195cce
gul <yusgul716@gmail.com>
2022-03-06 19:10:28 +0300

    guncel

---
+++ src/day3_If_Ternary/Q07.java
@@ -0,0 +1,19 @@
+package day3_If_Ternary;
+
+import java.util.Scanner;
+
+public class Q07 {
+
+	public static void main(String[] args) {
+		/*
+		 * Kullanicidan 1 harf (karakter) aliniz ve bunun buyuk harf, kucuk harf yada 
+		hatali giris seklinde kod yazniz
+		 */
+		 
+		
+		
+		
+
+	}
+
+}
